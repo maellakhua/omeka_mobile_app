caebdf69026eb8c9f3dce0a652132a1b77e0af21
Giannis Meletakis <meletakis@gmail.com>
2014-10-10 00:29:26 +0300

    Getting collection, collection details and item name(giannis)

--- momeka/www/js/app.js
+++ momeka/www/js/app.js
@@ -111,6 +111,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('dash');
 
 });
